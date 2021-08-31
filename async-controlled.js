89a5b41acc34a5ade09e8b96480e69cd9993c26f
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-08-31 10:41:56 +0600

    updated

--- async-controlled.js
+++ async-controlled.js
@@ -1,5 +1,27 @@
 
-
-const takeOrder = (customer) => {
+const takeOrder = (customer,callback) => {
     console.log(`take order for ${customer}`);
+    callback(customer);
 };
+
+const processOrder = (customer, callback) => {
+    console.log(`processing order for ${customer}`);
+
+    setTimeout(() => {
+        console.log(`Cooking Completed`);
+        console.log(`Order processed for ${customer}`);
+        callback(customer);
+    }, 3000);
+    console.log(`asdasd`)
+};
+
+const completeOrder = (customer) => {
+  console.log(`completed order for ${customer}`);
+};
+
+takeOrder('customer 1', (customer) => {
+    processOrder(customer, (customer) => {
+        completeOrder(customer);
+    });
+});
+
